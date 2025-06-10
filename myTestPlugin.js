df91276f91f1d65816b2b6826505c8f51132cf36
MrReeko <MrReekoGeared@gmail.com>
2025-06-09 21:53:35 -0400

    Update myTestPlugin.js

--- myTestPlugin.js
+++ myTestPlugin.js
@@ -1,22 +1,22 @@
-import { showNotification } from "@api/Notifications";
+const { showNotification } = Vencord.Webpack.findByProps("showNotification");
 
 const plugin = {
     name: "MyTestPlugin",
     start() {
-        console.log("MyTestPlugin started!");
-         showNotification({
-                title: "test",
-                body: `plugin loaded!`,
-                noPersist: true,
-            });
+        console.log("Plugin started");
+        showNotification?.({
+            title: "Plugin Test",
+            body: "Plugin loaded successfully",
+            noPersist: true,
+        });
     },
     stop() {
-        console.log("MyTestPlugin stopped!");
-         showNotification({
-                title: "test",
-                body: `plugin stopped!`,
-                noPersist: true,
-            });
+        console.log("Plugin stopped");
+        showNotification?.({
+            title: "Plugin Test",
+            body: "Plugin stopped",
+            noPersist: true,
+        });
     }
 };
 
