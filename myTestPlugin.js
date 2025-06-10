136fa6a22ab9206a75686a3e9b5818e2316460a5
MrReeko <MrReekoGeared@gmail.com>
2025-06-09 21:42:18 -0400

    1

--- myTestPlugin.js
+++ myTestPlugin.js
@@ -1,11 +1,13 @@
-export default {
-    name: "MyTestPlugin",
+const plugin = {
+    name: "MyPlugin",
     start() {
-        console.log("MyTestPlugin started!");
-        BdApi?.showToast?.("MyTestPlugin loaded!", { type: "success" });
+        console.log("MyPlugin started!");
+        BdApi?.showToast?.("MyPlugin loaded", { type: "success" });
     },
     stop() {
-        console.log("MyTestPlugin stopped!");
-        BdApi?.showToast?.("MyTestPlugin unloaded.", { type: "info" });
+        console.log("MyPlugin stopped!");
+        BdApi?.showToast?.("MyPlugin unloaded", { type: "info" });
     }
 };
+
+module.exports.default = plugin;
